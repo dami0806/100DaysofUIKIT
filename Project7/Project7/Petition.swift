58094e78d73ce8593513c19b4878d90cc54c9b4c
dami0806 <dami0804@naver.com>
2023-02-20 10:49:16 +0900

    Project7-1

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by 박다미 on 2023/02/20.
+//
+
+import Foundation
+
+struct Petition :Codable{
+    var title: String
+    var body : String
+    var signatureCount: Int
+}
---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,11 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by 박다미 on 2023/02/20.
+//
+
+import Foundation
+struct Petitions:Codable{
+    var results : [Petition]
+}
