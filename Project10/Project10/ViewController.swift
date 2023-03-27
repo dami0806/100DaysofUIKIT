c9afc688ac9689d6a25494c0176b23e99bae898c
dami0806 <dami0804@naver.com>
2023-03-27 16:01:58 +0900

    Project10-1

---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by 박다미 on 2023/03/27.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PersonCell", for: indexPath) as? PersonCell else {
+            // we failed to get a PersonCell – bail out!
+            fatalError("Unable to dequeue PersonCell.")
+        }
+
+        // if we're still here it means we got a PersonCell, so we can return it
+        return cell
+    }
+}
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // 원하는 셀 크기를 CGSize 형태로 반환합니다.
+        return CGSize(width: 140, height: 180)
+    }
+}
+
+
+
+
---
+++ Project9/Project9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project9
+//
+//  Created by 박다미 on 2023/03/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
